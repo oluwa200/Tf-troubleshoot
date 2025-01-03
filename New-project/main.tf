e1e4cbe767f30be638bebcc03da4d05138bf8401
oluwachei200 <daagnambodenon@gmail.com>
2025-01-02 19:56:26 -0500

    new change to provider

--- New-project/main.tf
+++ New-project/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "awoludaho012689" {
 
 
 # create an ec2
-resource "" "name" {
+resource "aws_ec2" "name" {
   resource "aws_instance" "web" {
   ami           = "01816d07b1128cd2d"
   instance_type = "t3.micro"
