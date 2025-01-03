d694de3f6bf61b42d3a769b989b7b2d7860118e2
oluwachei200 <daagnambodenon@gmail.com>
2025-01-02 19:29:21 -0500

    new commit

---
+++ New-project/main.tf
@@ -0,0 +1,22 @@
+# create an ec2 instance
+resource "aws_s3_bucket" "emeline01.02.2025" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+# create an ec2
+resource "" "name" {
+  resource "aws_instance" "web" {
+  ami           = "01816d07b1128cd2d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+}
---
+++ New-project/provider.tf
@@ -0,0 +1,5 @@
+# AWS Provider
+
+provider "AWS" {
+    region = "us-east-2"
+}
